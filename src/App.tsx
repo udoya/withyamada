1b20b387fd88156aeffac8b1a16fe3a87c1762f3
Yamada <yammtch@keio.jp>
2022-09-11 15:29:27 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1>Yamada</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
